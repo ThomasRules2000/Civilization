cdf2dcfdb1941776eadcbc0ce27ca5a12df971e6
ThomasRules2000 <tstacey2000@gmail.com>
2017-06-10 23:27:57 +0100

    Added Zoom Camera Rotation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
-    public Camera mainCamera;
-    public float zoomSpeed = 5000;
+    public float zoomSpeed = 1000;
     public float maxZoomedOut = 500;
     public float maxZoomedIn = 50;
+    public float topRotation = 90;
+    public float bottomRotation = 60;
+
+    float rotationStep;
 
     Pathfinding pathfinding;
     Seeker seeker;
@@ -16,10 +19,15 @@ public class Player : MonoBehaviour {
     {
         pathfinding = gameObject.GetComponent<Pathfinding>();
         seeker = pathfinding.seeker.GetComponent<Seeker>();
+
+        rotationStep = zoomSpeed * (topRotation-bottomRotation) / (maxZoomedOut - maxZoomedIn);
+        Camera.main.transform.position = new Vector3(Camera.main.transform.position.x,maxZoomedOut,Camera.main.transform.position.z);
+        Camera.main.transform.rotation = Quaternion.Euler(topRotation, Camera.main.transform.eulerAngles.y, Camera.main.transform.eulerAngles.z);
     }
 
     void Update ()
     {
+        //Mouse Click Pathfinding
         if (Input.GetMouseButtonUp(1))
         {
             seeker.moveSeeker = true;
@@ -29,22 +37,25 @@ public class Player : MonoBehaviour {
             seeker.moveSeeker = false;
             HandleInput();
         }
-        else
+
+        //Mouse Wheel Zoom
+        float d = Input.GetAxis("Mouse ScrollWheel");
+        if (d > 0f)
         {
-            float d = Input.GetAxis("Mouse ScrollWheel");
-            if (d > 0f)
+            //Zoom In
+            if (Camera.main.transform.position.y - zoomSpeed > maxZoomedIn)
             {
-                if (mainCamera.transform.position.y - zoomSpeed*Time.deltaTime > maxZoomedIn)
-                {
-                    mainCamera.transform.Translate(Vector3.Slerp(transform.position, new Vector3(transform.position.x, transform.position.y - zoomSpeed, transform.position.z), Time.deltaTime),Space.World);
-                }
+                Camera.main.transform.Translate(new Vector3(0, -zoomSpeed, zoomSpeed / (rotationStep * 40)), Space.World);
+                Camera.main.transform.rotation = Quaternion.Euler(Camera.main.transform.eulerAngles.x - rotationStep, Camera.main.transform.eulerAngles.y, Camera.main.transform.eulerAngles.z);
             }
-            else if (d < 0f)
+        }
+        else if (d < 0f)
+        {
+            //Zoom Out
+            if (Camera.main.transform.position.y + zoomSpeed < maxZoomedOut)
             {
-                if (mainCamera.transform.position.y + zoomSpeed*Time.deltaTime < maxZoomedOut)
-                {
-                    mainCamera.transform.Translate(Vector3.Slerp(transform.position, new Vector3(transform.position.x, transform.position.y + zoomSpeed, transform.position.z), Time.deltaTime),Space.World);
-                }
+                Camera.main.transform.Translate(new Vector3(0, zoomSpeed, -zoomSpeed / (rotationStep * 40)), Space.World);
+                Camera.main.transform.rotation = Quaternion.Euler(Camera.main.transform.eulerAngles.x + rotationStep, Camera.main.transform.eulerAngles.y, Camera.main.transform.eulerAngles.z);
             }
         }
 	}
