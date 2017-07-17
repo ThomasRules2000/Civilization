7f35680539d0daa2d2c6ef1493539a7e670b6694
ThomasRules2000 <tstacey2000@gmail.com>
2017-07-17 23:48:01 +0100

    Moved Cam Control to Separate Class

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,26 +5,13 @@ using UnityEngine.UI;
 
 public class Player : MonoBehaviour {
 
-    public float zoomSpeed = 18;
-    public float panMult = 180;
-    public float maxZoomedOut = 500;
-    public float maxZoomedIn = 50;
-    public float topRotation = 90;
-    public float bottomRotation = 60;
-
-    public float camTranslationSpeed = 20;
-    public float mouseScrollArea = 20;
-
-    public Transform cameraRig;
-    Camera[] cameras;
-
-    float rotationStep;
-
     int turnNo = 1;
     public Text turnCounterText;
     public Text civNameText;
 
     HexGrid grid;
+
+    public CameraRig cameraRig;
 
     public Unit unit;
     List<Unit> units = new List<Unit>();
@@ -53,16 +40,7 @@ public class Player : MonoBehaviour {
         turnCounterText.text = "Turn: " + turnNo;
 
 
-        cameras = cameraRig.GetComponentsInChildren<Camera>();
-        rotationStep = zoomSpeed * (topRotation-bottomRotation) / (maxZoomedOut - maxZoomedIn);
-        cameraRig.transform.position = new Vector3(unit.transform.position.x, maxZoomedIn, unit.transform.position.z - (maxZoomedIn / Mathf.Tan(Mathf.Deg2Rad * bottomRotation)));
-        cameraRig.transform.rotation = Quaternion.Euler(bottomRotation, cameraRig.transform.eulerAngles.y, cameraRig.transform.eulerAngles.z);
-
-        cameras[0].transform.localPosition = Vector3.zero;
-        cameras[0].transform.localRotation = Quaternion.identity;
-
-        cameras[1].transform.localPosition = Vector3.left * grid.width * HexMetrics.innerRad * 2;
-        cameras[1].transform.localRotation = Quaternion.identity;
+        
     }
 
     void Update ()
@@ -99,81 +77,13 @@ public class Player : MonoBehaviour {
         float scrollAxis = Input.GetAxis("Mouse ScrollWheel");
         if (scrollAxis > 0f)
         {
-            //Zoom In
-            if (cameraRig.transform.position.y - zoomSpeed >= maxZoomedIn)
-            {
-                Vector2 camMovement = Vector2.zero;
-
-                Ray inputRay = cameras[0].ScreenPointToRay(Input.mousePosition);
-                RaycastHit hit;
-                if (Physics.Raycast(inputRay, out hit))
-                {
-                    camMovement = new Vector2((hit.point.x - cameraRig.transform.position.x) * zoomSpeed / panMult, (hit.point.z - cameraRig.transform.position.z) * zoomSpeed / panMult);
-                }
-                else
-                {
-                    inputRay = cameras[1].ScreenPointToRay(Input.mousePosition);
-                    if (Physics.Raycast(inputRay, out hit))
-                    {
-                        camMovement = new Vector2((hit.point.x - cameraRig.transform.position.x) * zoomSpeed / panMult, (hit.point.z - cameraRig.transform.position.z) * zoomSpeed / panMult);
-                    }
-                }
-
-                cameraRig.transform.Translate(new Vector3(camMovement.x, -zoomSpeed, camMovement.y), Space.World);
-                cameraRig.transform.rotation = Quaternion.Euler(cameraRig.transform.eulerAngles.x - rotationStep, cameraRig.transform.eulerAngles.y, cameraRig.transform.eulerAngles.z);
-            }
+            cameraRig.ZoomIn();
         }
         else if (scrollAxis < 0f)
         {
             //Zoom Out
-            Vector2 camMovement = Vector2.zero;
-
-            Ray inputRay = cameras[0].ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-            if (Physics.Raycast(inputRay, out hit))
-            {
-                camMovement = new Vector2((cameraRig.transform.position.x - hit.point.x) * zoomSpeed / panMult, (cameraRig.transform.position.z - hit.point.z) * zoomSpeed / panMult);
-            }
-            else
-            {
-                inputRay = cameras[1].ScreenPointToRay(Input.mousePosition);
-                if (Physics.Raycast(inputRay, out hit))
-                {
-                    camMovement = new Vector2((cameraRig.transform.position.x - hit.point.x) * zoomSpeed / panMult, (cameraRig.transform.position.z - hit.point.z) * zoomSpeed / panMult);
-                }
-            }
-
-            if (cameraRig.transform.position.y + zoomSpeed <= maxZoomedOut)
-            {
-                cameraRig.transform.Translate(camMovement.x, +zoomSpeed, camMovement.y, Space.World);
-                cameraRig.transform.rotation = Quaternion.Euler(cameraRig.transform.eulerAngles.x + rotationStep, cameraRig.transform.eulerAngles.y, cameraRig.transform.eulerAngles.z);
-            }
+            cameraRig.ZoomOut();
         }
-
-        float horizAxis = Input.GetAxis("Horizontal") * Time.deltaTime * camTranslationSpeed * (cameraRig.transform.position.y - maxZoomedIn / 2);
-        float vertAxis = Input.GetAxis("Vertical") * Time.deltaTime * camTranslationSpeed * (cameraRig.transform.position.y - maxZoomedIn / 2);
-
-        float mPosX = Input.mousePosition.x;
-        float mPosY = Input.mousePosition.y;
-        if(mPosX >= 0 && mPosX < mouseScrollArea)
-        {
-            horizAxis = -Time.deltaTime * camTranslationSpeed * (cameraRig.transform.position.y - maxZoomedIn / 2);
-        }
-        else if(mPosX < Screen.width && mPosX >= Screen.width-mouseScrollArea)
-        {
-            horizAxis = Time.deltaTime * camTranslationSpeed * (cameraRig.transform.position.y - maxZoomedIn / 2);
-        }
-
-        if (mPosY >= 0 && mPosY < mouseScrollArea)
-        {
-            vertAxis = -Time.deltaTime * camTranslationSpeed * (cameraRig.transform.position.y - maxZoomedIn / 2);
-        }
-        else if (mPosY < Screen.height && mPosY >= Screen.height - mouseScrollArea)
-        {
-            vertAxis = Time.deltaTime * camTranslationSpeed * (cameraRig.transform.position.y - maxZoomedIn / 2);
-        }
-
-        cameraRig.transform.Translate(horizAxis, 0f, vertAxis, Space.World);
 	}
 
     public void NextTurn()
@@ -198,32 +108,16 @@ public class Player : MonoBehaviour {
 
     bool HandleInput(inputMethods method)
     {
-        Ray inputRay = cameras[0].ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
-        if (Physics.Raycast(inputRay, out hit))
+        if(cameraRig.Raycast(Input.mousePosition, out hit))
         {
-            if(method == inputMethods.doPath)
+            if (method == inputMethods.doPath)
             {
                 return DoPath(hit.point);
             }
-            else if(method == inputMethods.selectUnit)
+            else if (method == inputMethods.selectUnit)
             {
                 return SelectUnit(hit.transform);
-            }
-        }
-        else
-        {
-            inputRay = cameras[1].ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(inputRay, out hit))
-            {
-                if (method == inputMethods.doPath)
-                {
-                    return DoPath(hit.point);
-                }
-                else if (method == inputMethods.selectUnit)
-                {
-                    return SelectUnit(hit.transform);
-                }
             }
         }
         return false;
