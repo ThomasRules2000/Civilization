8b8be19a8438088f7ea1db046c4309f7b5082852
ThomasRules2000 <tstacey2000@gmail.com>
2017-10-13 10:10:11 +0100

    Began Adding Actions for GUI Buttons

---
+++ Assets/Scripts/Action.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Action
+{
+
+    public void CallAction()
+    {
+
+    }
+
+    public void AddAction()
+    {
+
+    }
+}
---
+++ Assets/Scripts/GUIControl.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GUIControl : MonoBehaviour {
+
+    int rightBuffer = 150; //Right buffer in pixels (due to minimap)
+    int buttonWidth = 50; //Width in pixels
+    Button defaultButton;
+    List<Button> buttons;
+
+	// Use this for initialization
+	void Start ()
+    {
+
+	}
+	
+    void updateButtons(List<Action> actions)
+    {
+        while(buttons.Count > 0)
+        {
+            Button btn = buttons[0];
+            Destroy(btn);
+            buttons.Remove(btn);
+        }
+
+        int lines = Mathf.CeilToInt(Screen.width / (buttonWidth * actions.Count));
+        for(int i = 0; i < lines; i++)
+        {
+            for(int j = 0; j < Mathf.CeilToInt((float)actions.Count / lines) && i * lines + j < actions.Count; j++)
+            {
+                Button button = Instantiate<Button>(defaultButton, new Vector3(i,j), Quaternion.identity, transform);
+                button.onClick.AddListener(actions[i*lines + j].CallAction);
+            }
+        }
+    }
+}
